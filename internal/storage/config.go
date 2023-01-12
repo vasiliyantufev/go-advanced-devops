4e139db1964b02f84cfd79b548e1d04bcb318eab
vasiliyantufev <vasiliyantufev@gmail.com>
2023-01-12 16:04:56 +0300

    config

--- internal/storage/config.go
+++ internal/storage/config.go
@@ -3,7 +3,14 @@ package storage
 import "time"
 
 type Config struct {
-	Address        string        `env:"ADDRESS"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL"`
-	PollInterval   time.Duration `env:"POLL_INTERVAL"`
+	Address        string        `env:"ADDRESS" 		envDefault:"localhost:8080"`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10s"`
+	PollInterval   time.Duration `env:"POLL_INTERVAL" 	envDefault:"2s"`
 }
+
+//type ServerConfig struct {
+//	Address string `env:"ADDRESS" envDefault:"localhost:8080"`
+//	StoreInterval time.Duration env:"STORE_INTERVAL" envDefault:"300s"
+//	StoreFile string env:"STORE_FILE"
+//	Restore bool env:"RESTORE" envDefault:"true"
+//}
