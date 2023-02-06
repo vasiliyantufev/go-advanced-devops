5ef6075d0269ae1f08d1daf561984702a82938ee
vasiliyantufev <vasiliyantufev@gmail.com>
2023-02-06 17:10:09 +0300

    flag

--- internal/storage/flags/flagsServer.go
+++ internal/storage/flags/flagsServer.go
@@ -6,23 +6,25 @@ var fgSrv flagsServer
 
 // Cтруктура хранения флагов
 type flagsServer struct {
-	a string //ADDRESS
-	f string //STORE_FILE
-	i string //STORE_INTERVAL
-	k string //KEY
-	d string //DATABASE_DSN
-	r bool   //RESTORE
+	a        string //ADDRESS
+	f        string //STORE_FILE
+	i        string //STORE_INTERVAL
+	k        string //KEY
+	d        string //DATABASE_DSN
+	r        bool   //RESTORE
+	buildvcs bool
 }
 
 // Реализация структуры для взаимодействия с ней
-func initFlagsServer(a, f, i, k, d *string, r *bool) flagsServer {
+func initFlagsServer(a, f, i, k, d *string, r, buildvcs *bool) flagsServer {
 	return flagsServer{
-		a: *a,
-		f: *f,
-		i: *i,
-		k: *k,
-		d: *d,
-		r: *r,
+		a:        *a,
+		f:        *f,
+		i:        *i,
+		k:        *k,
+		d:        *d,
+		r:        *r,
+		buildvcs: *buildvcs,
 	}
 }
 
@@ -34,9 +36,10 @@ func SetFlagsServer() {
 	restore := flag.Bool("r", true, "Булево значение, определяющее, загружать или нет начальные значения из указанного файла при старте сервера")
 	key := flag.String("k", "key", "Ключ для генерации хеша")
 	db := flag.String("d", "db", "База данных")
+	buildvcs := flag.Bool("buildvcs", false, "")
 
 	flag.Parse()
-	flags := initFlagsServer(address, file, interval, key, db, restore)
+	flags := initFlagsServer(address, file, interval, key, db, restore, buildvcs)
 
 	fgSrv.a = flags.a
 	fgSrv.f = flags.f
@@ -44,6 +47,7 @@ func SetFlagsServer() {
 	fgSrv.k = flags.k
 	fgSrv.r = flags.r
 	fgSrv.d = flags.d
+	fgSrv.buildvcs = flags.buildvcs
 }
 
 func GetFlagAddressServer() string {
