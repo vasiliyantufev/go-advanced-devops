24bd9da872dc8d6a44162810db4e48a87e2ab1e6
vasiliyantufev <4541435+vasiliyantufev@users.noreply.github.com>
2023-02-27 00:40:18 +0300

    refactoring agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,17 +2,12 @@ package main
 
 import (
 	"context"
-	"github.com/go-resty/resty/v2"
-	"os/signal"
-	"runtime"
-	"syscall"
-	"time"
-
 	"github.com/vasiliyantufev/go-advanced-devops/internal/app"
 	"github.com/vasiliyantufev/go-advanced-devops/internal/config"
 	"github.com/vasiliyantufev/go-advanced-devops/internal/storage"
+	"os/signal"
+	"syscall"
 
-	"github.com/shirou/gopsutil/v3/mem"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -21,11 +16,7 @@ func main() {
 	configAgent := config.NewConfigAgent()
 	memAgent := storage.NewMemStorage()
 	memAgentPsutil := storage.NewMemStorage()
-	//hashServer := &app.HashServer{}
 	hashServer := app.NewHashServer(configAgent.GetConfigKeyAgent())
-	urlPath := "http://" + configAgent.GetConfigAddressAgent() + "/updates/"
-
-	//log.Fatal(hashServer.Key)
 
 	ctx, cnl := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 	defer cnl()
@@ -33,96 +24,9 @@ func main() {
 	jobs := make(chan []storage.JSONMetrics, configAgent.RateLimit)
 	defer close(jobs)
 
-	go PutMetrics(ctx, memAgent, configAgent, hashServer)
-	go PutMetricsUsePsutil(ctx, memAgentPsutil, configAgent, hashServer)
-	go WrireMetricsToChan(ctx, jobs, memAgent, memAgentPsutil, configAgent)
-
-	for i := 0; i < configAgent.RateLimit; i++ {
-		go SentMetrics(ctx, jobs, urlPath)
-	}
+	agent := app.NewAgent(jobs, memAgent, memAgentPsutil, configAgent, hashServer)
+	agent.StartWorkers(ctx)
 
 	<-ctx.Done()
 	log.Println("agent shutdown on signal with:", ctx.Err())
-}
-
-func PutMetrics(ctx context.Context, memAgent *storage.MemStorage, cfg *config.ConfigAgent, hashServer *app.HashServer) {
-
-	ticker := time.NewTicker(cfg.GetConfigPollIntervalAgent())
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ctx.Done():
-			log.Println("poll ticker stopped by ctx")
-			return
-		case <-ticker.C:
-			log.Info("Put metrics")
-			stats := new(runtime.MemStats)
-			runtime.ReadMemStats(stats)
-			app.DataFromRuntime(memAgent, stats, hashServer)
-		}
-	}
-}
-
-func PutMetricsUsePsutil(ctx context.Context, memAgent *storage.MemStorage, cfg *config.ConfigAgent, hashServer *app.HashServer) {
-
-	ticker := time.NewTicker(cfg.GetConfigPollIntervalAgent())
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ctx.Done():
-			log.Println("poll psutil ticker stopped by ctx")
-			return
-		case <-ticker.C:
-			log.Info("Put metrics Psutil")
-			v, err := mem.VirtualMemory()
-			if err != nil {
-				log.Error(err)
-			}
-			app.DataFromRuntimeUsePsutil(memAgent, v, hashServer)
-		}
-	}
-}
-
-func WrireMetricsToChan(ctx context.Context, jobs chan []storage.JSONMetrics, agent *storage.MemStorage, psutil *storage.MemStorage, cfg *config.ConfigAgent) {
-
-	ticker := time.NewTicker(cfg.GetConfigReportIntervalAgent())
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ctx.Done():
-			log.Println("Read ticker stopped by ctx")
-			return
-		case <-ticker.C: // Запись в канал
-			log.Info("Write metrics to chan")
-			jobs <- agent.GetAllMetrics()
-			jobs <- psutil.GetAllMetrics()
-		}
-	}
-}
-
-func SentMetrics(ctx context.Context, jobs chan []storage.JSONMetrics, url string) {
-
-	client := resty.New()
-	for {
-		select {
-		case <-ctx.Done():
-			log.Println("Sent stopped by ctx")
-			return
-		case j := <-jobs:
-			makePostRequest(client, j, url)
-		}
-	}
-}
-
-func makePostRequest(client *resty.Client, j []storage.JSONMetrics, url string) {
-
-	_, err := client.R().
-		SetHeader("Content-Type", "application/json").
-		SetBody(j).
-		Post(url)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Println("Sent metrics success ", url)
 }
---
+++ internal/app/agent.go
@@ -0,0 +1,130 @@
+package app
+
+import (
+	"context"
+	"github.com/go-resty/resty/v2"
+	"github.com/shirou/gopsutil/v3/mem"
+	"runtime"
+	"time"
+
+	"github.com/vasiliyantufev/go-advanced-devops/internal/config"
+	"github.com/vasiliyantufev/go-advanced-devops/internal/storage"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Agent interface {
+	StartWorkers()
+	putMetricsWorker(ctx context.Context)
+	putMetricsUsePsutilWorker(ctx context.Context)
+	writeMetricsToChan(ctx context.Context)
+	sentMetricsWorker(ctx context.Context, url string)
+	makePostRequest(client *resty.Client, j []storage.JSONMetrics, url string)
+}
+
+type agent struct {
+	jobs       chan []storage.JSONMetrics
+	mem        *storage.MemStorage
+	psutil     *storage.MemStorage
+	cfg        *config.ConfigAgent
+	hashServer *HashServer
+}
+
+func NewAgent(jobs chan []storage.JSONMetrics, mem *storage.MemStorage, memPsutil *storage.MemStorage, cfg *config.ConfigAgent, hashServer *HashServer) *agent {
+	return &agent{jobs: jobs, mem: mem, psutil: memPsutil, cfg: cfg, hashServer: hashServer}
+}
+
+func (a agent) StartWorkers(ctx context.Context) {
+
+	urlPath := "http://" + a.cfg.GetConfigAddressAgent() + "/updates/"
+
+	go a.putMetricsWorker(ctx)
+	go a.putMetricsUsePsutilWorker(ctx)
+	go a.writeMetricsToChan(ctx)
+
+	for i := 0; i < a.cfg.RateLimit; i++ {
+		go a.sentMetrics(ctx, urlPath)
+	}
+}
+
+func (a agent) putMetricsWorker(ctx context.Context) {
+
+	ticker := time.NewTicker(a.cfg.GetConfigPollIntervalAgent())
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("poll ticker stopped by ctx")
+			return
+		case <-ticker.C:
+			log.Info("Put metrics")
+			stats := new(runtime.MemStats)
+			runtime.ReadMemStats(stats)
+			DataFromRuntime(a.mem, stats, a.hashServer)
+		}
+	}
+}
+
+func (a agent) putMetricsUsePsutilWorker(ctx context.Context) {
+
+	ticker := time.NewTicker(a.cfg.GetConfigPollIntervalAgent())
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("poll psutil ticker stopped by ctx")
+			return
+		case <-ticker.C:
+			log.Info("Put metrics Psutil")
+			v, err := mem.VirtualMemory()
+			if err != nil {
+				log.Error(err)
+			}
+			DataFromRuntimeUsePsutil(a.psutil, v, a.hashServer)
+		}
+	}
+}
+
+func (a agent) writeMetricsToChan(ctx context.Context) {
+
+	ticker := time.NewTicker(a.cfg.GetConfigReportIntervalAgent())
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Read ticker stopped by ctx")
+			return
+		case <-ticker.C: // Запись в канал
+			log.Info("Write metrics to chan")
+			a.jobs <- a.mem.GetAllMetrics()
+			a.jobs <- a.psutil.GetAllMetrics()
+		}
+	}
+}
+
+func (a agent) sentMetrics(ctx context.Context, url string) {
+
+	client := resty.New()
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Sent stopped by ctx")
+			return
+		case j := <-a.jobs:
+			a.makePostRequest(client, j, url)
+		}
+	}
+}
+
+func (a agent) makePostRequest(client *resty.Client, j []storage.JSONMetrics, url string) {
+
+	_, err := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(j).
+		Post(url)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("Sent metrics success ", url)
+}
